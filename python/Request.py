c9ab091f855b2a8e6a0df8b7500b0b3e86856104
Pranav Kumar <pmkumar@andrew.cmu.edu>
2018-09-08 15:53:11 -0400

    Integration working

--- python/Request.py
+++ python/Request.py
@@ -2,41 +2,34 @@ import requests
 import cognitive_sr
 import io
 
-# url = "https://westus.api.cognitive.microsoft.com/spid/v1.0/identificationProfiles/111f427c-3791-468f-b709-fcef7660fff9/enroll"
+def recognize_speaker(wav_data):
+    people = {
+        '2338f519-b782-4756-b0be-557fa731f1bf' : 'Pranav',
+        '818a16b4-8068-418a-88b5-d9f45a803e93' : 'Saiyan',
+        'b468f3aa-e303-4290-95ff-68a74524900e' : 'Jhosh' 
+    }
+    subscription_key = '38ca07d3e98a41889877dca1a68c884d'
 
-# files = {'file': open('test', 'rb')}
+    # Create a new profile
+    speech_identification = cognitive_sr.SpeechIdentification(subscription_key)
+    # result = speech_identification.create_profile()
+    # print(result)
+    profile_id = 'b468f3aa-e303-4290-95ff-68a74524900e'
+    # Pranav - 2338f519-b782-4756-b0be-557fa731f1bf
+    # Sayan - 818a16b4-8068-418a-88b5-d9f45a803e93
+    # Daddy Jhosh - b468f3aa-e303-4290-95ff-68a74524900e
 
-# headers = {'Ocp-Apim-Subscription-Key' : '38ca07d3e98a41889877dca1a68c884d', 'content-type' : 'multipart/form-data'}
-
-# r = requests.post(url, files=files, headers=headers)
-# print(r.text)
-people = {
-    '2338f519-b782-4756-b0be-557fa731f1bf' : 'Pranav',
-    '818a16b4-8068-418a-88b5-d9f45a803e93' : 'Saiyan',
-    'b468f3aa-e303-4290-95ff-68a74524900e' : 'Jhosh' 
-}
-subscription_key = '38ca07d3e98a41889877dca1a68c884d'
-
-# Create a new profile
-speech_identification = cognitive_sr.SpeechIdentification(subscription_key)
-# result = speech_identification.create_profile()
-# print(result)
-profile_id = 'b468f3aa-e303-4290-95ff-68a74524900e'
-# Pranav - 2338f519-b782-4756-b0be-557fa731f1bf
-# Sayan - 818a16b4-8068-418a-88b5-d9f45a803e93
-# Daddy Jhosh - b468f3aa-e303-4290-95ff-68a74524900e
-
-profile_ids = ['b468f3aa-e303-4290-95ff-68a74524900e', '2338f519-b782-4756-b0be-557fa731f1bf', '818a16b4-8068-418a-88b5-d9f45a803e93']
-wav_path = 'pranav_recording.wav'
-with io.open(wav_path, 'rb') as wav_file:
-    wav_data = wav_file.read()
-# result = speech_identification.enroll_profile(profile_id, wav_data)
-# result = speech_identification.delete_profile(profile_id)
-result = speech_identification.identify_profile(profile_ids, wav_data, short_audio=True)
-# result = speech_identification.get_profile(profile_id)
-profiles = speech_identification.get_all_profiles()
-print(profiles)
-print("Done")
-print(result)
-print('Identified wav as profile: ', people[result['identifiedProfileId']])
-print('Confidence is: ', result['confidence'])
+    profile_ids = ['b468f3aa-e303-4290-95ff-68a74524900e', '2338f519-b782-4756-b0be-557fa731f1bf', '818a16b4-8068-418a-88b5-d9f45a803e93']
+    wav_path = 'pranav_recording.wav'
+    # with io.open(wav_path, 'rb') as wav_file:
+    #     wav_data = wav_file.read()
+    # result = speech_identification.enroll_profile(profile_id, wav_data)
+    # result = speech_identification.delete_profile(profile_id)
+    result = speech_identification.identify_profile(profile_ids, wav_data, short_audio=True)
+    # result = speech_identification.get_profile(profile_id)
+    # profiles = speech_identification.get_all_profiles()
+    # print(profiles)
+    # print("Done")
+    # print(result)
+    if 'identifiedProfileId' in result.keys():
+        print(people[result['identifiedProfileId']], result['confidence'])
--- python/Voice_Detection.py
+++ python/Voice_Detection.py
@@ -1,18 +1,42 @@
+from threading import Thread
+from toxic import toxic_check
+
+import io
 import pyaudio
 import wave
 import webrtcvad
+
+import Request
+import STT
 
 CHUNK = 160
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
-RECORD_SECONDS = 3
+RECORD_SECONDS = 50
 WAVE_OUTPUT_FILENAME = "recording {wav_file_num}.wav"
+SILENT_FRAME_COUNT = 50
+
+# Thread to run speech to text in the background while listening for audio
+class SpeechToText(Thread):
+    def __init__(self, filename):
+        Thread.__init__(self)
+        self.filename = filename
+
+    def run(self):
+        speech = STT.speech_to_text(self.filename)
+        print(speech)
+        _, score = toxic_check(speech)
+        print(score)
+        with io.open(self.filename, 'rb') as wav_file:
+            wav_data = wav_file.read()
+        Request.recognize_speaker(wav_data)
+
 
 def main():
     p = pyaudio.PyAudio()
     vad = webrtcvad.Vad()
-    vad.set_mode(3)
+    vad.set_mode(0)
 
     stream = p.open(
         format=FORMAT,
@@ -32,26 +56,43 @@ def main():
         data = stream.read(CHUNK)
         frames.append(data)
         if (vad.is_speech(data, RATE)):
-            print('Contains speech:' + str(i))
+            # print('Contains speech:' + str(i))
             only_voice_frames.append(data)
             silent_frames = 0
         else:
+            # print("No speech")
             silent_frames += 1
-        if silent_frames >= 100:
-            print("100 frames of silence detected, writing frames so far to new wav file: ", WAVE_OUTPUT_FILENAME.format(wav_file_num=wav_file_num))
+        if silent_frames >= SILENT_FRAME_COUNT and len(only_voice_frames) > 40:
+            print(
+                "{count} frames of silence detected, writing {no_frames} frames so far to new wav file: ".format(
+                    count=SILENT_FRAME_COUNT, no_frames=len(only_voice_frames)
+                ), WAVE_OUTPUT_FILENAME.format(
+                    wav_file_num=wav_file_num
+                )
+            )
+
+            # Write audio so far to a wav file
             wf = wave.open(WAVE_OUTPUT_FILENAME.format(wav_file_num=wav_file_num), 'wb')
             wf.setnchannels(CHANNELS)
             wf.setsampwidth(p.get_sample_size(FORMAT))
             wf.setframerate(RATE)
             wf.writeframes(b''.join(only_voice_frames))
             wf.close()
+
+            # Create daemon thread to run speech to text for the wav file just generated
+            thread = SpeechToText(WAVE_OUTPUT_FILENAME.format(wav_file_num=wav_file_num))
+            thread.daemon = True
+            print("Starting thread to perform speech to text")
+            thread.start()
+
+            # update necessary values
             only_voice_frames = []
             silent_frames = 0
             wav_file_num += 1
 
 
     print("Done recording")
-    print(frames)
+    # print(frames)
     stream.stop_stream()
     stream.close()
     p.terminate()
