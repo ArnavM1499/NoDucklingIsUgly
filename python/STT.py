b045b661b7cc4bb4c1f674557653565e2443c81f
Pranav Kumar <pmkumar@andrew.cmu.edu>
2018-09-08 13:39:26 -0400

    Split continuous stream of audio into voice chunks, use Google STT

--- python/STT.py
+++ python/STT.py
@@ -6,22 +6,22 @@ import speech_recognition as sr
 
 # obtain audio from the microphone
 r = sr.Recognizer()
-file = sr.AudioFile('pranav_recording.wav')
+file = sr.AudioFile('recording 0.wav')
 with file as source:
     print("Say something!")
     audio = r.record(source)
 print(type(audio))
 
 # recognize speech using Google Speech Recognition
-# try:
-#     # for testing purposes, we're just using the default API key
-#     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-#     # instead of `r.recognize_google(audio)`
-#     print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-# except sr.UnknownValueError:
-#     print("Google Speech Recognition could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 # # recognize speech using Google Cloud Speech
 # GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
@@ -34,9 +34,9 @@ print(type(audio))
 
 # recognize speech using Microsoft Bing Voice Recognition
 BING_KEY = "dfc3671d31d8445497f47e9cdb301cc2"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+# try:
+#     print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
+# except sr.UnknownValueError:
+#     print("Microsoft Bing Voice Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
