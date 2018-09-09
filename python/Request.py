7814e4f2e5febdd4c311c8ac04daabf02accf76e
Pranav Kumar <pmkumar@andrew.cmu.edu>
2018-09-09 00:40:24 -0400

    Update

--- python/Request.py
+++ python/Request.py
@@ -24,11 +24,11 @@ def recognize_speaker(wav_data):
 
     profile_ids = ['5579c474-3c9e-4bdd-b1a6-9480c3d1366a', '2338f519-b782-4756-b0be-557fa731f1bf', '818a16b4-8068-418a-88b5-d9f45a803e93', '820b1301-cb96-4155-9f22-afc0b4d891cb']
     wav_path = 'jhosh_recording.wav'
-    with io.open(wav_path, 'rb') as wav_file:
-        wav_data = wav_file.read()
-    result = speech_identification.enroll_profile(profile_id, wav_data)
+    # with io.open(wav_path, 'rb') as wav_file:
+        # wav_data = wav_file.read()
+    # result = speech_identification.enroll_profile(profile_id, wav_data)
     # result = speech_identification.delete_profile(profile_id)
-    # result = speech_identification.identify_profile(profile_ids, wav_data, short_audio=True)
+    result = speech_identification.identify_profile(profile_ids, wav_data, short_audio=True)
     # result = speech_identification.get_profile(profile_id)
     # profiles = speech_identification.get_all_profiles()
     # print(profiles)
